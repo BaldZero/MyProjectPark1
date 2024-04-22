31b9a813a9a252cf871cdee5c8e77dd714fee782
Hyunmin Park <753429@student.dusd.net>
2024-04-22 15:28:36 -0700

    added basic movement and camera follow
    basic movement was added and the camera follows the p[layer

--- ProjectPark1/Assets/Scripts/PlayerControl.cs
+++ ProjectPark1/Assets/Scripts/PlayerControl.cs
@@ -8,6 +8,7 @@ public class PlayerControl : MonoBehaviour
     public float health = 100f;
     public Rigidbody rb;
 
+    public float mapBoundary = 25f;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,10 +18,27 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        /*float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");*/
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
 
-
+        transform.Translate(Vector3.right * horizontalInput * moveSpeed * Time.deltaTime);
+        transform.Translate(Vector3.forward * verticalInput * moveSpeed * Time.deltaTime);
         
+        if(transform.position.x > mapBoundary)
+        {
+            transform.position = new Vector3(mapBoundary, transform.position.y, transform.position.z);
+        }
+        if(transform.position.x < -mapBoundary)
+        {
+            transform.position = new Vector3(-mapBoundary, transform.position.y, transform.position.z);
+        }
+        if (transform.position.z > mapBoundary)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, mapBoundary);
+        }
+        if((transform.position.z < -mapBoundary))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -mapBoundary);
+        }
     }
 }
